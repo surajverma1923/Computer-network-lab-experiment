81d99b355075a722d0f021595246480b0fc6d3cf
surajverma1923 <104299654+surajverma1923@users.noreply.github.com>
2023-09-04 23:18:52 +0530

    client8.c

--- client8.c
+++ client8.c
@@ -1,63 +1,93 @@
+// client code for UDP socket programming
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <string.h>
-#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#define SIZE 1024
+#define IP_PROTOCOL 0
+#define IP_ADDRESS "127.0.0.1" // localhost
+#define PORT_NO 15050
+#define NET_BUF_SIZE 32
+#define cipherKey 'S'
+#define sendrecvflag 0
 
-void send_file(FILE *fp, int sockfd) {
-    int n;
-    char data[SIZE] = {0};
-
-    while (fgets(data, SIZE, fp) != NULL) {
-        if (send(sockfd, data, strlen(data), 0) == -1) { // Use strlen(data) instead of sizeof(data)
-            perror("[-]Error in sending file.");
-            exit(1);
-        }
-        bzero(data, SIZE);
-    }
+// function to clear buffer
+void clearBuf(char* b)
+{
+	int i;
+	for (i = 0; i < NET_BUF_SIZE; i++)
+		b[i] = '\0';
 }
 
-int main() {
-    char *ip = "127.0.0.1";
-    int port = 8080;
-    int e;
+// function for decryption
+char Cipher(char ch)
+{
+	return ch ^ cipherKey;
+}
 
-    int sockfd;
-    struct sockaddr_in server_addr;
-    FILE *fp;
-    char *filename = "send.txt";
+// function to receive file
+int recvFile(char* buf, int s)
+{
+	int i;
+	char ch;
+	for (i = 0; i < s; i++) {
+		ch = buf[i];
+		ch = Cipher(ch);
+		if (ch == EOF)
+			return 1;
+		else
+			printf("%c", ch);
+	}
+	return 0;
+}
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        perror("[-]Error in socket");
-        exit(1);
-    }
-    printf("[+]Server socket created successfully.\n");
+// driver code
+int main()
+{
+	int sockfd, nBytes;
+	struct sockaddr_in addr_con;
+	int addrlen = sizeof(addr_con);
+	addr_con.sin_family = AF_INET;
+	addr_con.sin_port = htons(PORT_NO);
+	addr_con.sin_addr.s_addr = inet_addr(IP_ADDRESS);
+	char net_buf[NET_BUF_SIZE];
+	FILE* fp;
 
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(port); // Use htons to convert port to network byte order
-    server_addr.sin_addr.s_addr = inet_addr(ip);
+	// socket()
+	sockfd = socket(AF_INET, SOCK_DGRAM,
+					IP_PROTOCOL);
 
-    e = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
-    if (e == -1) {
-        perror("[-]Error in connecting to server"); // Update error message
-        exit(1);
-    }
-    printf("[+]Connected to Server.\n");
+	if (sockfd < 0)
+		printf("\nfile descriptor not received!!\n");
+	else
+		printf("\nfile descriptor %d received\n", sockfd);
 
-    fp = fopen(filename, "r");
-    if (fp == NULL) {
-        perror("[-]Error in opening file."); // Update error message
-        exit(1);
-    }
+	while (1) {
+		printf("\nPlease enter file name to receive:\n");
+		scanf("%s", net_buf);
+		sendto(sockfd, net_buf, NET_BUF_SIZE,
+			sendrecvflag, (struct sockaddr*)&addr_con,
+			addrlen);
 
-    send_file(fp, sockfd);
-    printf("[+]File data sent successfully.\n");
+		printf("\n---------Data Received---------\n");
 
-    printf("[+]Closing the connection.\n");
-    close(sockfd);
+		while (1) {
+			// receive
+			clearBuf(net_buf);
+			nBytes = recvfrom(sockfd, net_buf, NET_BUF_SIZE,
+							sendrecvflag, (struct sockaddr*)&addr_con,
+							&addrlen);
 
-    return 0;
+			// process
+			if (recvFile(net_buf, NET_BUF_SIZE)) {
+				break;
+			}
+		}
+		printf("\n-------------------------------\n");
+	}
+	return 0;
 }
